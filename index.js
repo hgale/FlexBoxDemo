f1d0cc9ac0345077f5449f10faf5bd1251a73fe7
Harry Gale <harry.n.gale@gmail.com>
2018-01-16 15:26:14 -0800

    Start flexbox demo

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+console.ignoredYellowBox = ['Remote debugger'];
+console.disableYellowBox = true;
+
 AppRegistry.registerComponent('FlexBoxDemo', () => App);
